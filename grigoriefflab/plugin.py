017cfa58028a3df3d0fbd78b3a054916cbb347d9
Yaiza Rancel <rancelyaiza@gmail.com>
2018-04-30 15:01:37 +0200

    add latest ctffind bin version

--- grigoriefflab/plugin.py
+++ grigoriefflab/plugin.py
@@ -33,6 +33,9 @@ def registerPluginBinaries(env):
                    default=True  # Optional: install this binary by default
                    )
 
+    env.addPackage('ctffind4', version='4.1.10',
+                   tar='ctffind4-4.1.10.tgz')
+
     env.addPackage('summovie', version='1.0.2',
                    tar='summovie_1.0.2.tgz')
 
